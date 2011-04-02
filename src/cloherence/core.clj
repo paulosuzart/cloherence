6e2ea796db4595b42db95335cff5321cd2657a18
paulosuzart <paulosuzart@gmail.com>
2011-04-01 21:59:53 -0300

    Added licence

--- src/cloherence/core.clj
+++ src/cloherence/core.clj
@@ -1,4 +1,15 @@
-(ns cloherence.core
+; Copyright (c) Paulo Suzart. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file epl-v10.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+
+
+(ns ^{:doc "core clojerence abstraction."
+      :author "Paulo Suzart"}
+     cloherence.core
 	(:use [clojure.contrib.def :only [name-with-attributes]])
 	(:import [com.tangosol.net CacheFactory NamedCache]
 			 [com.tangosol.util.processor AbstractProcessor]))
